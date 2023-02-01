bca1aa9ad2b939427c259e57ec9ef64ce0c2e354
agambewe <agambewe@gmail.com>
2023-02-01 12:53:03 +0700

    update storage

--- monitoring-system.sh
+++ monitoring-system.sh
@@ -8,13 +8,23 @@ echo -n -e "    Hostname \t "
 hostname
 echo -n -e "    Ip Address \t "
 hostname -I
-echo -n -e "    Storage \t "
-df -hl | awk '/^\/dev\/sd[abc]/ { sum+=$5 } END { print sum"%" }'
+echo
+
 echo -n -e "    CPU \t "
 top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}'
 echo -n -e "    Memory \t "
 free | grep Mem | awk '{printf("%.2f%", $3/$2 * 100.0)}'
-# echo -e "%"
+echo
+
+#used
+USED=`df -l | awk '/^\/dev\/mapper\/*/ { sum+=$3 } END { print sum }'`
+#avail
+AVAIL=`df -l | awk '/^\/dev\/mapper\/*/ { sum+=$2 } END { print sum }'`
+
+TOTAL=`echo "$USED $AVAIL" | awk '{print ($1/$2*100)}'`
+
+echo -n -e "    Storage \t "
+printf "%0.2f%% \n" $TOTAL
 
 sleep 3
 done
